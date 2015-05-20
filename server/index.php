f9e804251783d51ed586f7e17d21fffcd00cb8db
arehfeldt <arehfeldt@riotgames.com>
2015-05-20 11:50:18 -0700

    fix syntax errors

--- server/index.php
+++ server/index.php
@@ -2,9 +2,10 @@
   $action = !empty($_REQUEST['action']) ? $_REQUEST['action'] : false;
 
   $logfile = fopen('server.log', 'a+');
-
+  
 
   if ($action===false) {
+    echo "hey now";
     exit;
   }
 
@@ -17,6 +18,7 @@
     // Put photos somewhere to be composited and printed
     case "done":
       break;
+  }
 
 
   // Upload photos from photopi
@@ -31,22 +33,33 @@
     print_r($_FILES);
     exit;
 
-
-
     // Generate hash for these photos
     $code = md5(time());
     
-
     // Figure out what the last one was
     $last = file_get_contents('lastside');
     $next = ($last == 'BLUE') ? 'PINK' : 'BLUE';
+
+    $dirname = '/home/pi/purikura/server/photos/'.$next.'/'.$code;
+
+    // Make directory for the photos
+    mkdir($dirname);
+
+    // Move photos into the directory
+    foreach ($_FILES['photos']['name'] as $tmppath) {
+      $pieces = explode("/", $tmppath);
+      $filename = array_pop($pieces);
+
+      $newpath = $dirname."/".$filename;
+      move_uploaded_file($tmppath, $newpath);
+    }
     
     file_put_contents('last', $next);
     logstuff ("Photo - created code $code - sent to $next");
 
     output(array(
       'code' => $code,
-      'side' => $next)
+      'side' => $next
     ));
   }
 
